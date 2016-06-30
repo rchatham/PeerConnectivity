42b929e49664067570875db28b37538fdd42d905
Reid Chatham <reid.chatham@gmail.com>
2016-06-30 15:30:07 -0700

    carthage compatibility

---
+++ Carthage/Build/iOS/PeerConnectivity.framework/Headers/PeerConnectivity.h
@@ -0,0 +1,19 @@
+//
+//  PeerConnectivity.h
+//  PeerConnectivity
+//
+//  Created by Reid Chatham on 6/9/16.
+//  Copyright © 2016 Reid Chatham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PeerConnectivity.
+FOUNDATION_EXPORT double PeerConnectivityVersionNumber;
+
+//! Project version string for PeerConnectivity.
+FOUNDATION_EXPORT const unsigned char PeerConnectivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeerConnectivity/PublicHeader.h>
+
+
