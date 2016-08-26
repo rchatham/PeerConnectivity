5c28859db6e5e93ac1d6773518c6f7456d9b0839
Reid Chatham <reid.chatham@gmail.com>
2016-08-25 22:52:27 -0700

    Swift package manager support

--- PeerConnectivityTests/PeerConnectivityTests.swift
+++ PeerConnectivityTests/PeerConnectivityTests.swift
@@ -11,14 +11,22 @@ import XCTest
 
 class PeerConnectivityTests: XCTestCase {
     
+    var pcm: PeerConnectionManager?
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        pcm = PeerConnectionManager(serviceType: "test-service")
+        pcm?.start()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        pcm?.stop()
+        pcm = nil
     }
     
     func testExample() {
