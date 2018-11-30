52cac75927ec21bd44096dc275b24ffc0ef1ca5e
Julien Di Marco <juliendimarco@gmail.com>
2018-11-30 16:54:01 +0100

    Adds/Use Atomic type/container for Observable.value attribute, improve thread-safety on library

--- Sources/MultiObservable.swift
+++ Sources/MultiObservable.swift
@@ -10,26 +10,40 @@ import Foundation
 
 internal class MultiObservable<T> {
     internal typealias Observer = (T) -> Void
+
+    // MARK: - Properties
+
     internal var observers: [String:Observer] = [:]
     
+    private let _value: Atomic<T>
+    internal var value: T {
+        set {
+            _value.set(newValue)
+
+            for (_, observer) in observers {
+                observer(newValue)
+            }
+        }
+        get {
+            return _value.value
+        }
+    }
+
+    // MAKRK: - Initializers -
+
+    internal init(_ value: T) {
+        _value = Atomic<T>(value: value)
+    }
+
+    // MARK: - Observers
+
     internal func addObserver(_ observer: @escaping Observer, key: String) {
         observer(value)
         self.observers[key] = observer
     }
-    
+
     internal func removeObserverForkey(_ key: String) {
         self.observers.removeValue(forKey: key)
     }
-    
-    internal var value: T {
-        didSet {
-            for (_, observer) in observers {
-                observer(value)
-            }
-        }
-    }
-    
-    internal init(_ v: T) {
-        value = v
-    }
+
 }
