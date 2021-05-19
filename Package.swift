5d5d68c3c6288d89168230d27fffe3da53e2a7b6
Geoffrey <geoffrey.thenot@gmail.com>
2021-05-19 16:40:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,4 +2,29 @@
 
 import PackageDescription
 
-let package = Package(name: "PeerConnectivity")
+let package = Package(
+    name: "PeerConnectivity",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "PeerConnectivity",
+            targets: ["PeerConnectivity"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "PeerConnectivity",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "PeerConnectivityTests",
+            dependencies: ["PeerConnectivity"],
+            path: "PeerConnectivityTests"
+        ),
+    ]
+)
