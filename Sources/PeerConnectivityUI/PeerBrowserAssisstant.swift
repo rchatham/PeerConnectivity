f5af0d881cdfde6edec5daf88585d7d68e35873a
Reid Chatham <reid.chatham@gmail.com>
2026-08-07 10:14:35 -0700

    fix(ui): retain browser delegate without subclassing

--- Sources/PeerConnectivityUI/PeerBrowserAssisstant.swift
+++ Sources/PeerConnectivityUI/PeerBrowserAssisstant.swift
@@ -10,23 +10,10 @@ import Foundation
 import MultipeerConnectivity
 import PeerConnectivity
 #if canImport(UIKit)
+import ObjectiveC
 import UIKit
 
-internal final class PeerBrowserViewController: MCBrowserViewController {
-
-    fileprivate let eventProducer: PeerBrowserViewControllerEventProducer
-
-    internal init(serviceType: ServiceType, session: MCSession, eventProducer: PeerBrowserViewControllerEventProducer) {
-        self.eventProducer = eventProducer
-        super.init(serviceType: serviceType, session: session)
-        delegate = eventProducer
-    }
-
-    @available(*, unavailable)
-    required internal init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+fileprivate var PeerBrowserViewControllerEventProducerKey : UInt8 = 0
 
 internal struct PeerBrowserAssisstant {
 
@@ -41,7 +28,15 @@ internal struct PeerBrowserAssisstant {
     internal func peerBrowserViewController(_ callback: @escaping (PeerBrowserViewControllerEvent) -> Void,
                                             peerFilter: PeerBrowserViewControllerPeerFilter? = nil) -> MCBrowserViewController {
         let eventProducer = PeerBrowserViewControllerEventProducer(callback: callback, peerFilter: peerFilter)
-        return PeerBrowserViewController(serviceType: serviceType, session: session, eventProducer: eventProducer)
+        let browserViewController = MCBrowserViewController(serviceType: serviceType, session: session)
+        browserViewController.delegate = eventProducer
+        objc_setAssociatedObject(
+            browserViewController,
+            &PeerBrowserViewControllerEventProducerKey,
+            eventProducer,
+            .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+        )
+        return browserViewController
     }
 }
 #endif
